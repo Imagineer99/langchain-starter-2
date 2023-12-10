62cc0a6d0b06234470b82858dbdba728b0cc56b2
Lee Jackson <130007945+Imagineer99@users.noreply.github.com>
2023-12-10 19:02:09 +0000

    Update 8 files

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,15 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import { Message as VercelChatMessage, StreamingTextResponse } from "ai";
-
-import { ChatOpenAI } from "langchain/chat_models/openai";
+import { StreamingTextResponse } from "ai";
 import { BytesOutputParser } from "langchain/schema/output_parser";
 import { PromptTemplate } from "langchain/prompts";
 
 export const runtime = "edge";
-
-const formatMessage = (message: VercelChatMessage) => {
-  return `${message.role}: ${message.content}`;
-};
 
 const TEMPLATE = `You are a pirate named Patchy. All responses must be extremely verbose and in pirate dialect.
 
@@ -19,49 +13,75 @@ Current conversation:
 User: {input}
 AI:`;
 
-/**
- * This handler initializes and calls a simple chain with a prompt,
- * chat model, and output parser. See the docs for more information:
- *
- * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
- */
+async function makeFireworksApiRequest(messages, input) {
+  try {
+    const apiKey = "Fni3tw7mZupBiApZWyvGeWBdEiJJRpQEFmlBSltenldaRDdP";
+
+    const WebSocket = require("ws");
+    const isServer = typeof window === "undefined";
+    const socket = new WebSocket(
+      "wss://api.fireworks.ai/inference/v1/chat/completions",
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      },
+    );
+
+    return new Promise((resolve, reject) => {
+      socket.onmessage = (event) => {
+        const eventData = JSON.parse(event.data);
+        resolve(eventData);
+        socket.close();
+      };
+
+      socket.onerror = (error) => {
+        reject(error);
+      };
+
+      // Send the request
+      socket.send(
+        JSON.stringify({
+          model: "accounts/fireworks/models/mistral-7b-instruct-4k",
+          messages: [
+            {
+              role: "user",
+              content: "",
+            },
+            ...messages, // Include previous messages
+          ],
+          stream: true,
+          n: 1,
+          max_tokens: 150,
+          temperature: 0.1,
+          top_p: 0.9,
+        }),
+      );
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    const messages = body.messages ?? [];
-    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
-    const currentMessageContent = messages[messages.length - 1].content;
+    const messages = body.messages || [];
+    const formattedPreviousMessages = messages.map(
+      (message) => `${message.role}: ${message.content}`,
+    );
+    const currentMessageContent = body.input || "";
     const prompt = PromptTemplate.fromTemplate(TEMPLATE);
-    /**
-     * You can also try e.g.:
-     *
-     * import { ChatAnthropic } from "langchain/chat_models/anthropic";
-     * const model = new ChatAnthropic({});
-     *
-     * See a full list of supported models at:
-     * https://js.langchain.com/docs/modules/model_io/models/
-     */
-    const model = new ChatOpenAI({
-      temperature: 0.8,
-    });
-    /**
-     * Chat models stream message chunks rather than bytes, so this
-     * output parser handles serialization and byte-encoding.
-     */
+
+    // Make a request to the Fireworks.ai API
+    const fireworksApiResponse = await makeFireworksApiRequest(
+      formattedPreviousMessages,
+      currentMessageContent,
+    );
+
+    // Process the response as needed
     const outputParser = new BytesOutputParser();
-
-    /**
-     * Can also initialize as:
-     *
-     * import { RunnableSequence } from "langchain/schema/runnable";
-     * const chain = RunnableSequence.from([prompt, model, outputParser]);
-     */
-    const chain = prompt.pipe(model).pipe(outputParser);
-
-    const stream = await chain.stream({
-      chat_history: formattedPreviousMessages.join("\n"),
-      input: currentMessageContent,
-    });
+    const stream = outputParser.transform(fireworksApiResponse);
 
     return new StreamingTextResponse(stream);
   } catch (e: any) {
